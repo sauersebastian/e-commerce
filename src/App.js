41334cb32678c91180df8857d8c578f4e85fe9d9
Sebastian Sauer <sebauerr@gmail.com>
2020-08-29 20:59:56 -0300

    prueba1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
-import Home from './components/home';
 
 function App() {
   return (
@@ -13,14 +12,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
