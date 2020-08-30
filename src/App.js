0c5d335bf251a716749220d898bcb4be226b68d4
Sebastian Sauer <sebauerr@gmail.com>
2020-08-29 21:41:43 -0300

    Home.jsx con prop

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
         <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </header>
+      <main>
+          <Home greeting="Te paso un prop!" />
+        </main>
     </div>
   );
 }
---
+++ src/components/home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import './home.scss';
+
+
+
+class Home extends React.Component {
+    render(){
+        const { greeting = ''} = this.props;
+
+        return <div className="home">
+            <Card border="dark" style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>Paso un parametro</Card.Title>
+                <Card.Text>
+                    {greeting}
+                </Card.Text>
+                <Card.Link href="https://google.com.ar">Un link</Card.Link>
+            </Card.Body>
+            </Card>
+        </div>
+    }
+}
+
+export default Home;
