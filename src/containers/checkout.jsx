133c3405dc7df5174d34cf69cbd5d94caceceffa
Sebastian Sauer <sebauerr@gmail.com>
2020-10-20 12:33:04 -0300

    Mejoras form checkout

--- src/containers/checkout.jsx
+++ src/containers/checkout.jsx
@@ -1,46 +1,22 @@
 import React, { useState } from 'react';
 import { Form, Col, Button } from 'react-bootstrap';
-//import Button from "../components/Button/Button";
 import { useCartContext } from '../context/cartContext';
 import * as firebase from 'firebase/app';
 import { getFirestore } from '../firebase/';
+import { useForm } from "react-hook-form";
 
 export default function Checkout() {
 
     const { cart, totalPrice } = useCartContext();
-
+    const { register, handleSubmit } = useForm();
     const [loading, setLoading] = useState(false);
-    const [userInfo, setUserInfo] = useState({
-        nameuser: " ",
-        lastname: " ",
-        email: " ",
-        phone: " ",
-        adress1: " ",
-        adress2: " ",
-        country: " ",
-        zipcode: " ",
-        city: " ",
-        province: " "
-    });
+    
 
-    function handleChange(evt) {
-        const value = evt.target.value;
-        setUserInfo({
-          ...userInfo,
-          [evt.target.name]: value
-        });
-        
-        evt.persist()
-    }
-
-    console.log("HOLACART " + cart);
-
-    function handleSubmit(evt) {
-
+    const onSubmit = ((user) => {
         const db = getFirestore();
         const orders = db.collection("orders");
         const newOrder = {
-            buyer: userInfo,
+            buyer: user,
             items: cart,
             total: totalPrice(),
             date: firebase.firestore.Timestamp.fromDate(new Date()),
@@ -57,71 +33,71 @@ export default function Checkout() {
             setLoading(true);
         });
 
-    }
+    })
 
     function CheckoutOrder() {
 
         return (
             <div>
-                <Form>
+                <Form onSubmit={handleSubmit(onSubmit)}>
                     <Form.Row>
                         <Form.Group as={Col} controlId="formGridName">
                             <Form.Label>Nombres</Form.Label>
-                            <Form.Control type="name" name="nameuser" value={userInfo.nameuser} onChange={handleChange} />
+                            <Form.Control type="name" name="nameuser" ref={register}/>
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridLastName">
                             <Form.Label>Apellidos</Form.Label>
-                            <Form.Control type="lastname" name="lastname" value={userInfo.lastname} onChange={handleChange} />
+                            <Form.Control type="lastname" name="lastname" ref={register}/>
                         </Form.Group>
                     </Form.Row>
 
                     <Form.Row>
                         <Form.Group as={Col} controlId="formGridEmail">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" name="email" value={userInfo.email} onChange={handleChange} />
+                            <Form.Control type="email" name="email" ref={register}/>
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridPhone">
                             <Form.Label>Telefono</Form.Label>
-                            <Form.Control type="phone" name="phone" value={userInfo.phone} onChange={handleChange} />
+                            <Form.Control type="phone" name="phone" ref={register}/>
                         </Form.Group>
                     </Form.Row>
 
                     <Form.Group controlId="formGridAddress1">
                         <Form.Label>Dirección</Form.Label>
-                        <Form.Control name="adress1" placeholder="Nombre de la calle y altura" value={userInfo.adress1} onChange={handleChange} />
+                        <Form.Control name="adress1" placeholder="Nombre de la calle y altura" ref={register}/>
                     </Form.Group>
 
                     <Form.Group controlId="formGridAddress2">
-                        <Form.Control name="adress2" placeholder="Departamento, suite, unidad, etc. (opcional)" value={userInfo.adress2} onChange={handleChange} />
+                        <Form.Control name="adress2" placeholder="Departamento, suite, unidad, etc. (opcional)" ref={register}/>
                     </Form.Group>
 
                     <Form.Row>
                         <Form.Group as={Col} controlId="formGridCountry">
                             <Form.Label>País</Form.Label>
-                            <Form.Control name="country" value={userInfo.country} onChange={handleChange} />
+                            <Form.Control name="country" ref={register}/>
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridZip">
                             <Form.Label>Código Postal</Form.Label>
-                            <Form.Control name="zipcode" value={userInfo.zipcode} onChange={handleChange} />
+                            <Form.Control name="zipcode" ref={register}/>
                         </Form.Group>
                     </Form.Row>
 
                     <Form.Row>
                         <Form.Group as={Col} controlId="formGridCity">
                             <Form.Label>Localidad/Ciudad</Form.Label>
-                            <Form.Control name="city" value={userInfo.city} onChange={handleChange} />
+                            <Form.Control name="city" ref={register}/>
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridState">
                             <Form.Label>Provincia</Form.Label>
-                            <Form.Control name="province" value={userInfo.province} onChange={handleChange} />
+                            <Form.Control name="province" ref={register}/>
                         </Form.Group>
                     </Form.Row>
 
-                    <Button onClick={handleSubmit}>COMPRAR AHORA</Button>
+                    <Button type="submit">COMPRAR AHORA</Button>
                 </Form>
             </div>
         );
